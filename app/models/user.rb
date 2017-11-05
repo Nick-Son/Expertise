afcd85e4857ddc60b37f9793864c0eb445ec808d
Nick Sonner <nicksonner@gmail.com>
2017-11-05 14:51:08 +1100

    add pundit gem to gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_one :profile
-  has_many :expertise_areas
+  has_and_belongs_to_many :expertise_areas
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,

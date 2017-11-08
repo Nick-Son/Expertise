0f891865fed19f78996e0b9700985185fafb836f
Nick Sonner <nicksonner@gmail.com>
2017-11-08 16:17:08 +1100

    updated readme

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -17,19 +17,14 @@ class BookingsController < ApplicationController
   # GET /bookings/1
   # GET /bookings/1.json
   def show
+
   end
 
   # GET /bookings/new
   def new
     @expert = User.find(params[:expert_id])
-    # @client = current_user
-
     @booking = Booking.new
-    #@booking.expert_id = Profile.find(params[:expert_id])
-    # @booking.client_id = current_user.id
-    # @booking.expert_id = @expert.id 
 
-    #@booking.expert = User.find(params[:user_id])
   end
 
   # GET /bookings/1/edit
@@ -40,53 +35,48 @@ class BookingsController < ApplicationController
   # POST /bookings.json
   def create
     @booking = Booking.new(booking_params)
-
-    #  temp_params = params.require(:booking).permit(:expert_id)
-    #  @expert = User.find(temp_params[:expert_id])
-
-    #@expert = User.find(params[:expert_id])
-    #@client = current_user
-    
     @booking.client = current_user
-    rate_per_hour = booking_params[:rate_per_hour]
-    duration = booking_params[:duration]
 
-    # Shrine
+    # Stripe
+    #byebug
+    rate_per_hour = @booking.rate_per_hour
+    duration = @booking.duration
     @amount = rate_per_hour.to_i * duration.to_i
-  
+    
     customer = Stripe::Customer.create(
       :email => current_user.email,
       :source  => params[:stripeToken]
-    )
-  
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => "Session with #{@booking.expert.profile.first_name}",
-      :currency    => 'aud'
-    )
+      )
+      
+      charge = Stripe::Charge.create(
+        :customer    => customer.id,
+        :amount      => @amount,
+        :description => "Session with #{@booking.expert.profile.first_name}",
+        :currency    => 'aud'
+        )
+        
+        @booking.charge_identifier = charge.id
+        @booking.save
+        redirect_to @booking, notice: 'Booking was successfully payed.'
+        
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_bookings_path(expert_id: booking_params[:expert_id])
+        
+        #Redirecting if saved or not saved
+        # respond_to do |format|
+        #   if @booking.save
+        #     format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
+        #     format.json { render :show, status: :created, location: @booking }
+        #   else
+        #     format.html { render :new }
+        #     format.json { render json: @booking.errors, status: :unprocessable_entity }
+        #   end
+        # end
 
-    @booking.charge_identifier = charge.id
-    @booking.save
-    redirect_to @booking, notice: 'Booking was successfully created.'
-
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_bookings_path(expert_id: @booking.expert.id)
-
-    # Redirecting if saved or not saved
-    # respond_to do |format|
-    #   if @booking.save
-    #     format.html { redirect_to @booking, notice: 'Booking was successfully created.' }
-    #     format.json { render :show, status: :created, location: @booking }
-    #   else
-    #     format.html { render :new }
-    #     format.json { render json: @booking.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  end
-
-  # PATCH/PUT /bookings/1
+      end
+      
+      # PATCH/PUT /bookings/1
   # PATCH/PUT /bookings/1.json
   def update
     respond_to do |format|
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     resources :messages
   end 
 
+  #post '/bookings' 
+
   get '/support' => 'support#new'
   post '/support' => 'support#create'
 
